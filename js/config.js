b0068f7c0ea7b3e40122875da2e4c0d35e1756a4
CB Harsha Vardhan <harshacb922@gmail.com>
2025-07-20 12:41:27 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_zYb8Xhyad', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: ' 3q15oo8m7hjrrhm85c6kkrphvk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
