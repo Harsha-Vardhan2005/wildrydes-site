a7407f0222c6166d74988d501126ffe74c6b81cf
CB Harsha Vardhan <harshacb922@gmail.com>
2025-07-20 12:45:51 +0530

    Update2 config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_zYb8Xhyad', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: ' 3q15oo8m7hjrrhm85c6kkrphvk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3q15oo8m7hjrrhm85c6kkrphvk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
